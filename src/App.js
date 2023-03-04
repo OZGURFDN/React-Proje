23cd320ebc056e2cf7f2eb86a28a042c71e0e595
ozgrfdn@gmail.com <ozgrfdn@gmail.com>
2023-03-04 12:54:35 +0300

    image Eklendi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import Course from "./Course";
+import Angular from "./images/angular.jpg";
+import bootstrap5 from "./images/bootstrap5.png";
+import ccsharp from "./images/ccsharp.png";
+import kompleweb from "./images/kompleweb.jpg";
 
 function App() {
   return (
     <div className="App">
       <Course
+      image={Angular}
         title="Angular"
         description="
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum unde
@@ -12,7 +17,9 @@ function App() {
           rem inventore magnam maiores et dolore accusamus ad placeat
           repudiandae animi minima?"
       />
+
       <Course
+        image={bootstrap5}
         title="Bootstrap 5"
         description="
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum unde
@@ -20,14 +27,18 @@ function App() {
           rem inventore magnam maiores et dolore accusamus ad placeat
           repudiandae animi minima?"
       />
+
       <Course
+        image={ccsharp}
         title="Komple Web"
         description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum unde
           alias laboriosam minima! Omnis, quisquam. Optio doloremque deserunt,
           rem inventore magnam maiores et dolore accusamus ad placeat
           repudiandae animi minima?"
       />
+
       <Course
+        image={kompleweb}
         title="Frontend"
         description="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum unde
           alias laboriosam minima! Omnis, quisquam. Optio doloremque deserunt,
